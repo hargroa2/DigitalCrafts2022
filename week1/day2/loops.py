3ea1f6e3352bbfa4d26386c076538f23deec543e
Amanda Hargrove <amandahargrove98@gmail.com>
2022-06-07 18:59:16 -0400

    Updated my notes so that the code wasn't commented out

--- week1/day2/loops.py
+++ week1/day2/loops.py
@@ -8,29 +8,29 @@
 #while (condition to evaluate):
 #condition can be checked many times
 
-# data = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-# num = 0
-# while 2 < len(data): # Gives you the length of a list
-#     print(data[num])
-#     num += 1
+data = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+num = 0
+while 2 < len(data): # Gives you the length of a list
+    print(data[num])
+    num += 1
 
 #for loop
-# fruits = ["apple", "pineapple", "banana", "apple", "pineapple", "banana"]
-# #for singularItem in list
-# for fruit in fruits: #will print out as many items you have
-#     print(fruit)
-#     if fruit == "banana":
-#         pass
+fruits = ["apple", "pineapple", "banana", "apple", "pineapple", "banana"]
+#for singularItem in list
+for fruit in fruits: #will print out as many items you have
+    print(fruit)
+    if fruit == "banana":
+        pass
 
 #We use lists inside of for loops primarily
 
-# data = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-#names = ["student1", "student2", "student3"]
-# num = 0
-# for fruit in fruits:
-#     for name in names:
-#         num +=1
-#         print(name)
+data = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+names = ["student1", "student2", "student3"]
+num = 0
+for fruit in fruits:
+    for name in names:
+        num +=1
+        print(name)
 
 # print(num) 
 #generally, you don't have to do nested for loops. There are usually better solutions, but are good for traversing strings. 
--- week1/day2/python101practice.py
+++ week1/day2/python101practice.py
@@ -1,83 +1,90 @@
 #Small 1.
-# name = input("What is your name? ")
-# print("Hello, " + name + "!")
+name = input("What is your name? ")
+print("Hello, " + name + "!")
 
 #Small 2.
-# name2 = input("WHAT IS YOUR NAME? ")
-# nameLength = len(name2)
-# print("HELLO, " + name2.upper() + "!")
-# print("YOUR NAME HAS " + str(nameLength) + " CHARACTERS!")
+name2 = input("WHAT IS YOUR NAME? ")
+nameLength = len(name2)
+print("HELLO, " + name2.upper() + "!")
+print("YOUR NAME HAS " + str(nameLength) + " CHARACTERS!")
 
 #Small 3.
-# print("Please fill in the blanks below: ")
-# print("___(name)___'s favorite subject is ___(subject)___.")
-# theName = input("What is the name? ")
-# theSubject = input("What is your favorite subject? ")
-# print(theName + "'s favorite animal is a " + theSubject)
+print("Please fill in the blanks below: ")
+print("___(name)___'s favorite subject is ___(subject)___.")
+theName = input("What is the name? ")
+theSubject = input("What is your favorite subject? ")
+print(theName + "'s favorite animal is a " + theSubject)
 
-#Small 4.
-# day = int(input("Day (0-6)?"))
-# match day:
-#     case "0":
-#         print("Sunday")
-#     case "1":
-#         print("Monday")
-#     case "2":
-#         print("Tuesday")
-#     case "3":
-#         print("Wednesday")
-#     case "4":
-#         print("Thursday")
-#     case "5":
-#         print("Friday")
-#     case "6":
-#         print("Saturday")
+#Small 4. (Didn't have to do this, was just curious)
+day = int(input("Day (0-6)?"))
+match day:
+    case "0":
+        print("Sunday")
+    case "1":
+        print("Monday")
+    case "2":
+        print("Tuesday")
+    case "3":
+        print("Wednesday")
+    case "4":
+        print("Thursday")
+    case "5":
+        print("Friday")
+    case "6":
+        print("Saturday")
 
-# print(day)
+print(day)
 
 #Small 9.
-# for i in range(5):
-#     print("*****")
+for i in range(5):
+    print("*****")
 
 #Small 10.
-# squareSize = int(input("How big"))
-# for i in range(squareSize): #Run as many times as square size is
-#     print("*" * squareSize)
+squareSize = int(input("How big"))
+for i in range(squareSize): #Run as many times as square size is
+    print("*" * squareSize)
 
 # Medium 1.
-# bill = float(input("What was your bill? "))
-# tip = float(input("What tip would you like to give? Put it in a percentage. "))
+bill = float(input("What was your bill? "))
+tip = float(input("What tip would you like to give? Put it in a percentage. "))
 
-# def tipCalc():
-#     calcTip = bill * (tip*0.01)
-#     calcBill = bill + calcTip
-#     print("Your total bill is " + str(calcBill))
+def tipCalc():
+    calcTip = bill * (tip*0.01)
+    calcBill = bill + calcTip
+    print("Your total bill is " + str(calcBill))
 
-# tipCalc()
+tipCalc()
 
 # Large 3. Guessing number game
 # give me a number starting from 1 and 10 and figure out a way to store a random number and compare the answer to what the input is
 
-# while True:
-#     print("I am thinking of a number between 1 and 10.")
-#     import random
-#     randomNumber = random.randint(1,10)
-#     guess = int(input("Enter a number from 1 to 10: "))
-#     if guess < randomNumber:
-#         print("Guess is too low")
-#         guess = int(input("Enter a number from 1 to 10: "))
-#     elif guess > randomNumber:
-#         print("Guess is too high")
-#         guess = int(input("Enter a number from 1 to 10: "))
-#     if guess == randomNumber: 
-#         print("Yes! You win!")
-#         playAgain = input("Do you want to play again (Y or N)? ")
-#         if playAgain == "Y":
-#             continue
-#         if playAgain == "N":
-#             print("Bye!")
-#             break
+while True:
+    print("I am thinking of a number between 1 and 10.")
+    import random
+    randomNumber = random.randint(1,10)
+    guess = int(input("Enter a number from 1 to 10: "))
+    if guess < randomNumber:
+        print("Guess is too low")
+        guess = int(input("Enter a number from 1 to 10: "))
+    elif guess > randomNumber:
+        print("Guess is too high")
+        guess = int(input("Enter a number from 1 to 10: "))
+    if guess == randomNumber: 
+        print("Yes! You win!")
+        playAgain = input("Do you want to play again (Y or N)? ")
+        if playAgain == "Y":
+            continue
+        if playAgain == "N":
+            print("Bye!")
+            break
 
+x = 0
+while True:
+    if x == 20:
+        break
+    x += 1
+    print(x)
+# loops up until 20, printing 1-20 and then breaking
 
 
 
