436533b410a78f889bdf79049fd30ed86919cc1c
Amanda Hargrove <amandahargrove98@gmail.com>
2022-07-06 00:47:13 -0400

    Uploading Weather App

---
+++ week5/day1/promises.js
@@ -0,0 +1,66 @@
+//A promise is an action you take and you wait on an expected result. It will either be what you wanted or not. Either resolve or reject
+//This is asynchronous, which means that even if you invoke it it will take some time to get back to you
+
+function getCoffee(type) {
+  console.log("coffee");
+}
+
+//async function, async action, promise
+// const oliviasCoffee = async getTypeofCoffee()
+// getCoffee(oliviasCcoffee)
+
+//two ways to handle promises
+//.then
+//async await ES6
+getCoffeePromise = new Promise((resolve, reject) => {
+  const coffee = "blonde roast";
+  if (coffee === "black") {
+    resolve("I have your black coffee");
+  } else {
+    reject("I do not have black coffee");
+  }
+});
+
+getCoffeePromise
+  .then((message) => console.log(message))
+  .catch((error) => console.log(error)); //will trigger if promise is rejected
+
+async function order() {
+  try {
+    const coffeeTime = await getCoffeePromise; //can only use in an async function
+    console.log(coffeeTime);
+  } catch (error) {
+    console.log(error);
+  }
+}
+order();
+
+//statuses: 200 is okay
+//look up webfx http status codes glossary
+
+// APIS
+/*
+Go to webdocs fetch() function to see structure. Fetching data from a place that is not local for our use
+CRUD: Create read update destroy
+ */
+const functionName = async () => {
+  const container = document.getElementById("container");
+
+  //fetch from somewhere
+  const url = "https://api.com";
+  //you must await your fetch
+  const response = await fetch(url); //node does not have fetch yet. if you run node with fetch in your project, it won't work
+  //you have to convert it to json
+  const json = await response.json();
+  console.log(json.results); //must look at the data to know what to use
+
+  for (const monster of json.results) {
+    pokemonCard.classList = "pokemon-card";
+    const pokemonName = document.createElement("h1");
+    pokemonName.innerText = "pokemon name";
+    pokemonCard.appendChild(pokemonName);
+    container.appendChild(pokemonCard);
+  }
+};
+
+functionName();
---
+++ week5/day1/weatherApp/weather.js
@@ -0,0 +1,82 @@
+const API_KEY = "ff1cadec65d9f10c3aafc2a01b14612c";
+
+//button
+const searchButton = document.getElementById("temp");
+const contain = document.getElementById("container");
+const dataContain = document.getElementById("dataContainer");
+
+const tempNumber = document.getElementById("tempNum");
+const feelsNumber = document.getElementById("feelsNum");
+const windNumber = document.getElementById("windNum");
+const humidNumber = document.getElementById("humidNum");
+
+const searchWeather = async () => {
+  const searchInput = document.getElementById("location").value;
+
+  const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${searchInput},us&appid=${API_KEY}&units=imperial`;
+
+  const weatherData = await fetch(forecastUrl);
+  const json = await weatherData.json();
+  console.log(json);
+
+  //temperature append
+  const showTemp = document.createElement("p");
+  showTemp.style.fontSize = "35px";
+  showTemp.innerText = json.list[20].main.temp + "\u00B0F";
+  tempNumber.append(showTemp);
+
+  //feelslike append
+  const showFeel = document.createElement("p");
+  showFeel.style.fontSize = "35px";
+  showFeel.innerText = json.list[0].main.feels_like + "\u00B0F";
+  feelsNumber.append(showFeel);
+
+  //wind append
+  const showWind = document.createElement("p");
+  showWind.style.fontSize = "35px";
+  showWind.innerText = json.list[0].wind.speed + "mph";
+  windNumber.append(showWind);
+
+  //humid append
+  const showHumid = document.createElement("p");
+  showHumid.style.fontSize = "35px";
+  showHumid.innerText = json.list[0].main.humidity + "%";
+  humidNumber.append(showHumid);
+
+  const monNumber = document.getElementById("monNum");
+  const tuesNumber = document.getElementById("tuesNum");
+  const wedNumber = document.getElementById("wedNum");
+  const thursNumber = document.getElementById("thursNum");
+  const friNumber = document.getElementById("friNum");
+
+  //Monday
+  const showMon = document.createElement("p");
+  showMon.style.fontSize = "35px";
+  showMon.innerText = json.list[4].main.temp + "\u00B0F";
+  monNumber.append(showMon);
+
+  //Tuesday
+  const showTues = document.createElement("p");
+  showTues.style.fontSize = "35px";
+  showTues.innerText = json.list[12].main.temp + "\u00B0F";
+  tuesNumber.append(showTues);
+
+  //Wednesday
+  const showWed = document.createElement("p");
+  showWed.style.fontSize = "35px";
+  showWed.innerText = json.list[20].main.temp + "\u00B0F";
+  wedNumber.append(showWed);
+
+  //Thursday
+  const showThurs = document.createElement("p");
+  showThurs.style.fontSize = "35px";
+  showThurs.innerText = json.list[28].main.temp + "\u00B0F";
+  thursNumber.append(showThurs);
+
+  //Friday
+  const showFri = document.createElement("p");
+  showFri.style.fontSize = "35px";
+  showFri.innerText = json.list[36].main.temp + "\u00B0F";
+  friNumber.append(showFri);
+};
+searchButton.onclick = () => searchWeather();
