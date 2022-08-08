09824fb65fbb5263c9ed7ad103c2cb9b9f4bb74f
Amanda Hargrove <amandahargrove98@gmail.com>
2022-08-08 15:30:46 -0400

    Uploading rest of notes for today

---
+++ week10/day1/dist/assignment2.js
@@ -0,0 +1,74 @@
+"use strict";
+const userIds = [
+  {
+    id: "b6809de6-1743-11ed-861d-0242ac120002",
+  },
+  {
+    id: "b680a0b6-1743-11ed-861d-0242ac120002",
+  },
+  {
+    id: "b680a1e2-1743-11ed-861d-0242ac120002",
+  },
+  {
+    id: "b680a2fa-1743-11ed-861d-0242ac120002",
+  },
+  {
+    id: "b680a3fe-1743-11ed-861d-0242ac120002",
+  },
+  {
+    id: "b680a7c8-1743-11ed-861d-0242ac120002",
+  },
+  {
+    id: "b680a8ea-1743-11ed-861d-0242ac120002",
+  },
+  {
+    id: "b680aa0c-1743-11ed-861d-0242ac120002",
+  },
+  {
+    id: "b680ab2e-1743-11ed-861d-0242ac120002",
+  },
+];
+const usersToAssign = [
+  { name: "user" },
+  { name: "user" },
+  { name: "user" },
+  { name: "user" },
+  { name: "user" },
+  { name: "user" },
+  { name: "user" },
+  { name: "user" },
+  { name: "user" },
+];
+let someone = {
+  name: usersToAssign[0].name,
+  dept: "software",
+  id: userIds[0].id,
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  userVerification: (name, id) => {
+    if (name && id) {
+      return true;
+    }
+    return false;
+  },
+};
+// 3 functions
+const listOfNames = ["sam", "john", "joe"];
+let replaceNames = () => {
+  for (let i = 0; i < usersToAssign.length; i++) {
+    usersToAssign[i].name = listOfNames[i];
+  }
+};
+replaceNames();
+// assign IDs to users
+// replace all users with the name "user" to a random name generated
+// format the user to send to DB
+// userVerifcation func
+// I should be able to pass the name of the user and the id, and it should match one of these id's and the name should match one of the names in your list
+// const userVerication = (id, name) => {
+// check if the name is in the name list
+// check if the id is in the id list
+// if it passes all test, it returns true, else false
+// };
+
+console.log("hi");
---
+++ week10/day1/notes.md
@@ -0,0 +1,9 @@
+tsc --init
+
+- Makes typescript file
+
+tsc
+
+- Compiles and makes JS file from .ts file
+
+- Work in src folder (you create this yourself off to the side), outputs in dist folder (automatically made via tsc)
